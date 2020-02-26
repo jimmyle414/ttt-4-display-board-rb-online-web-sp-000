98d9f213099311084473b0604e1047eacd79618b
Jimmy Le <jimmyle414@gmail.com>
2020-02-26 21:50:12 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -26,26 +26,26 @@ end
 #def display_board(board= [" ", " ", " ", " ", "X", " ", " ", " ", " "])
   #board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
 
-puts "A board with an X in the center position"
+#puts "A board with an X in the center position"
 
-      puts "  #{board[0]}| #{board[1]} |#{board[2]}  "
-      puts "-----------"
-      puts "  #{board[3]}| #{board[4]} |#{board[5]}  "
-      puts "-----------"
-      puts "  #{board[6]}| #{board[7]} |#{board[8]}  "
-end
+      #puts "  #{board[0]}| #{board[1]} |#{board[2]}  "
+      #puts "-----------"
+      #puts "  #{board[3]}| #{board[4]} |#{board[5]}  "
+      #puts "-----------"
+      #puts "  #{board[6]}| #{board[7]} |#{board[8]}  "
+#end
 
-display_board
+#display_board
 
-def display_board(board= ["O", " ", " ", " ", " ", " ", " ", " ", " "])
-board = ["O", " ", " ", " ", " ", " ", " ", " ", " "]
-puts "A board with O in the top left"
+#def display_board(board= ["O", " ", " ", " ", " ", " ", " ", " ", " "])
+#board = ["O", " ", " ", " ", " ", " ", " ", " ", " "]
+#puts "A board with O in the top left"
 
-      puts "  #{board[0]}| #{board[1]} |#{board[2]}  "
-      puts "-----------"
-      puts "  #{board[3]}| #{board[4]} |#{board[5]}  "
-      puts "-----------"
-      puts "  #{board[6]}| #{board[7]} |#{board[8]}  "
-end
+      #puts "  #{board[0]}| #{board[1]} |#{board[2]}  "
+      #puts "-----------"
+      #puts "  #{board[3]}| #{board[4]} |#{board[5]}  "
+      #puts "-----------"
+      #puts "  #{board[6]}| #{board[7]} |#{board[8]}  "
+#end
 
-display_board
+#display_board
