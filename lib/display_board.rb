5f9e417be7e80a9742c9bb39dc9e5cde301e0420
Jimmy Le <jimmyle414@gmail.com>
2020-02-26 20:54:08 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -25,3 +25,15 @@ def display_board(board = [" "," "," "," ","X "," "," "," "," "])
 end
 
 display_board
+
+def display_board(board = ["O"," "," "," "," "," "," "," "," "])
+
+
+      puts "  #{board[0]}| #{board[1]} |#{board[2]}  "
+      puts "-----------"
+      puts "  #{board[3]}| #{board[4]} |#{board[5]}  "
+      puts "-----------"
+      puts "  #{board[6]}| #{board[7]} |#{board[8]}  "
+end
+
+display_board
