bb7022371a878bde0fe2e223351bdc6c8ab10baf
Jimmy Le <jimmyle414@gmail.com>
2020-02-26 20:44:13 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,7 +3,7 @@
 
 def display_board(board = [" "," "," "," "," "," "," "," "," "])
 
-puts 'display_board'
+puts 'A blank board '
       puts "  #{board[0]}| #{board[1]} |#{board[2]} "
       puts "-----------"
       puts "  #{board[3]}| #{board[4]} |#{board[5]} "
