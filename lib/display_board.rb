05503e60b4bb6010294d72e02cb0f05fe3cd755a
Jimmy Le <jimmyle414@gmail.com>
2020-02-26 21:39:08 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -31,3 +31,11 @@ display_board
 
 def display_board(board= ["O", " ", " ", " ", " ", " ", " ", " ", " "])
 board = ["O", " ", " ", " ", " ", " ", " ", " ", " "]
+puts "A board with an X in the center position"
+
+      puts "  #{board[0]}| #{board[1]} |#{board[2]}  "
+      puts "-----------"
+      puts "  #{board[3]}| #{board[4]} |#{board[5]}  "
+      puts "-----------"
+      puts "  #{board[6]}| #{board[7]} |#{board[8]}  "
+end
